c53f19e8e946eb0879bccd0a2c4627687afd8b64
jp.levesque <jean-philippe.levesque@nventive.com>
2022-08-04 12:11:57 -0400

    feat: Add IViewModelViewFactory and its implementation.

---
+++ src/DynamicMvvm.Uno/ViewModel/ViewModelViewFactory.cs
@@ -0,0 +1,15 @@
+﻿using Windows.UI.Xaml;
+
+namespace Chinook.DynamicMvvm
+{
+	/// <summary>
+	/// This implementation of <see cref="IViewModelViewFactory"/> uses the <see cref="ViewModelView"/> implementation.
+	/// </summary>
+	public class ViewModelViewFactory : IViewModelViewFactory
+	{
+		public IViewModelView Create(object view)
+		{
+			return new ViewModelView((FrameworkElement)view);
+		}
+	}
+}
