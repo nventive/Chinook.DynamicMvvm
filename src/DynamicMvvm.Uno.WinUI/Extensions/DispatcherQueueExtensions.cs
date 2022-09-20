27069081aa5aab7965ec05af2799609d9f5e8658
Gabriel Chouinard Létourneau <gabriel.chouinard@nventive.com>
2022-09-20 11:41:55 -0400

    feat: update for .NET 6 support

---
+++ src/DynamicMvvm.Uno.WinUI/Extensions/DispatcherQueueExtensions.cs
@@ -0,0 +1,96 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// https://github.com/CommunityToolkit/WindowsCommunityToolkit/blob/main/License.md
+// See reference: https://github.com/CommunityToolkit/WindowsCommunityToolkit/blob/main/Microsoft.Toolkit.Uwp/Extensions/DispatcherQueueExtensions.cs
+
+using System;
+using System.Threading.Tasks;
+using Microsoft.UI.Dispatching;
+using System.Runtime.CompilerServices;
+
+namespace Chinook.DynamicMvvm.Extensions
+{
+	/// <summary>
+	/// This class exposes extensions methods on <see cref="DispatcherQueue"/>.
+	/// </summary>
+	internal static class DispatcherQueueExtensions
+	{
+		/// <summary>
+		/// Invokes a given function on the target <see cref="DispatcherQueue"/> and returns a
+		/// <see cref="Task"/> that completes when the invocation of the function is completed.
+		/// </summary>
+		/// <param name="taskCompletionSource">The <see cref="TaskCompletionSource"/> that will set the result or Exception depending on the result of the invocation</param>
+		/// <param name="handler">The <see cref="DispatcherQueueHandler"/> to invoke.</param>
+		/// <returns>A <see cref="DispatcherQueueHandler"/> that completes when the invocation of <paramref name="handler"/> is over.</returns>
+		internal static DispatcherQueueHandler InvokeHandler(DispatcherQueueHandler handler, TaskCompletionSource<object?> taskCompletionSource)
+		{
+			try
+			{
+				handler.Invoke();
+
+				taskCompletionSource.SetResult(null);
+			}
+			catch (Exception e)
+			{
+				taskCompletionSource.SetException(e);
+			}
+
+			return handler;
+		}
+
+		/// <summary>
+		/// Invokes a given function on the target <see cref="DispatcherQueue"/> and returns a
+		/// <see cref="Task"/> that completes when the invocation of the function is completed.
+		/// </summary>
+		/// <param name="dispatcher">The target <see cref="DispatcherQueue"/> to invoke the code on.</param>
+		/// <param name="handler">The <see cref="DispatcherQueueHandler"/> to invoke.</param>
+		/// <param name="priority">The priority level for the function to invoke.</param>
+		/// <returns>A <see cref="Task"/> that completes when the invocation of <paramref name="handler"/> is over.</returns>
+		/// <remarks>If the current thread has access to <paramref name="dispatcher"/>, <paramref name="handler"/> will be invoked directly.</remarks>
+		internal static Task RunAsync(this DispatcherQueue dispatcher, DispatcherQueuePriority priority = DispatcherQueuePriority.Normal, DispatcherQueueHandler handler = default)
+		{
+			// Run the function directly when we have thread access.
+			// Also reuse Task.CompletedTask in case of success,
+			// to skip an unnecessary heap allocation for every invocation.
+			if (dispatcher.HasThreadAccess)
+			{
+				try
+				{
+					handler.Invoke();
+
+					return Task.CompletedTask;
+				}
+				catch (Exception e)
+				{
+					return Task.FromException(e);
+				}
+			}
+
+			return TryEnqueueAsync(dispatcher, handler, priority);
+		}
+
+		internal static Task TryEnqueueAsync(DispatcherQueue dispatcher, DispatcherQueueHandler handler, DispatcherQueuePriority priority)
+		{
+			var taskCompletionSource = new TaskCompletionSource<object>();
+			DispatcherQueueHandler callback = InvokeHandler(handler, taskCompletionSource);
+
+			if (!dispatcher.TryEnqueue(priority, callback))
+			{
+				taskCompletionSource.SetException(GetEnqueueException("Failed to enqueue the operation"));
+			}
+
+			return taskCompletionSource.Task;
+		}
+
+		/// <summary>
+		/// Creates an <see cref="InvalidOperationException"/> to return when an enqueue operation fails.
+		/// </summary>
+		/// <param name="message">The message of the exception.</param>
+		/// <returns>An <see cref="InvalidOperationException"/> with a specified message.</returns>
+		[MethodImpl(MethodImplOptions.NoInlining)]
+		internal static InvalidOperationException GetEnqueueException(string message)
+		{
+			return new InvalidOperationException(message);
+		}
+	}
+}
---
+++ src/DynamicMvvm.Uno/Dispatchers/CoreDispatcherDispatcher.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using Windows.UI.Xaml;
+using Windows.UI.Core;
+using Microsoft.Extensions.Logging;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace Chinook.DynamicMvvm
+{
+	/// <summary>
+	/// This implementation of <see cref="IDispatcher"/> uses <see cref="CoreDispatcher"/>.
+	/// </summary>
+	public class CoreDispatcherDispatcher : IDispatcher
+	{
+		private readonly CoreDispatcher _coreDispatcher;
+
+		/// <summary>
+		/// Initializes a new instance of the <see cref="CoreDispatcherDispatcher"/> class.
+		/// </summary>
+		/// <param name="frameworkElement">The <see cref="FrameworkElement"/> from which to retrieve the <see cref="CoreDispatcher"/>.</param>
+		public CoreDispatcherDispatcher(FrameworkElement frameworkElement)
+		{
+			if (frameworkElement is null)
+			{
+				throw new ArgumentNullException(nameof(frameworkElement));
+			}
+
+			_coreDispatcher = frameworkElement.Dispatcher;
+		}
+
+		/// <summary>
+		/// Initializes a new instance of the <see cref="CoreDispatcherDispatcher"/> class.
+		/// </summary>
+		/// <param name="coreDispatcher">The <see cref="CoreDispatcher"/>.</param>
+		public CoreDispatcherDispatcher(CoreDispatcher coreDispatcher)
+		{
+			if (coreDispatcher is null)
+			{
+				throw new ArgumentNullException(nameof(coreDispatcher));
+			}
+
+			_coreDispatcher = coreDispatcher;
+		}
+
+		/// <inheritdoc />
+		public bool GetHasDispatcherAccess() => _coreDispatcher.HasThreadAccess;
+
+		/// <inheritdoc />
+		public async Task ExecuteOnDispatcher(CancellationToken ct, Action action)
+		{
+			if (ct.IsCancellationRequested)
+			{
+				this.Log().LogDebug($"Cancelled 'ExecuteOnDispatcher' because of the cancellation token.");
+				return;
+			}
+
+			await _coreDispatcher.RunAsync(CoreDispatcherPriority.High, () =>
+			{
+				try
+				{
+					if (ct.IsCancellationRequested)
+					{
+						this.Log().LogDebug($"Cancelled 'ExecuteOnDispatcher' because of the cancellation token.");
+						return;
+					}
+
+					action();
+				}
+				catch (Exception e)
+				{
+					this.Log().LogError(e, "Failed 'ExecuteOnDispatcher'.");
+				}
+			});
+		}
+	}
+}
